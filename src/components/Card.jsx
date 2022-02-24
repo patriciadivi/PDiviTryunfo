833981dd67c2ac78d77fa0ae9f9b15e2e3cf119b
PatriciaDivi <patriciavianad@gmail.com>
2022-02-24 17:48:28 -0300

    3 Requisito | Crie e renderize o componente Card com as props necessárias | finalizado | :rocket:

---
+++ src/components/Card.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
+import '../styles/SectionCard.css';
+
+class Card extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
+    return (
+      <section className="SectionCard">
+        <Title titleContainer="Adicionar uma novo Card" />
+        <section>
+          <p data-testid="name-card">{ cardName }</p>
+          <img
+            src={ cardImage }
+            alt={ cardName }
+            data-testid="image-card"
+          />
+          <p data-testid="description-card">{ cardDescription }</p>
+          <p data-testid="attr1-card">{ cardAttr1 }</p>
+          <p data-testid="attr2-card">{ cardAttr2 }</p>
+          <p data-testid="attr3-card">{ cardAttr3 }</p>
+          <p data-testid="rare-card">{ cardRare }</p>
+          {(cardTrunfo) ? <p data-testid="trunfo-card">Super Trunfo</p> : ''}
+        </section>
+      </section>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
